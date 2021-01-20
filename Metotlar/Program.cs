2f1e12a4efff2dd7696ec1c3975ab2291eecf5b5
özkan göktaş <karozkan@yahoo.com>
2021-01-20 21:34:22 +0300

    Proje dosyası ekle.

---
+++ Metotlar/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // string urunAdi = "Elma";
+            // double fiyati = 15;
+            // string aciklama = "Amasya Elmasi";
+            // string[] meyveler = new string[] { "Elma", "Karpuz" };
+
+            Urun urun1 = new Urun();
+            urun1.Adi = ("Elma");
+            urun1.Fiyati = 15;
+            urun1.Aciklama = "Amasya Elmasi";
+
+            Urun urun2 = new Urun();
+            urun2.Adi = ("Karpuz");
+            urun2.Fiyati = 80;
+            urun2.Aciklama = "Diyerbakir karpuzu";
+
+            Urun[] urunler = new Urun[] { urun1, urun2 };
+
+            // type safe = tip guvenli
+            foreach (var Urun in urunler)
+            {
+                Console.WriteLine(Urun.Adi);
+                Console.WriteLine(Urun.Fiyati);
+                Console.WriteLine(Urun.Aciklama);
+                Console.WriteLine("===============");
+            }
+
+            Console.WriteLine("------Metotlar-----");
+
+            // instance = ornek
+            SepetManager sepetManager = new SepetManager();
+            sepetManager.Ekle(urun1);
+            sepetManager.Ekle(urun2);
+
+            sepetManager.Ekle2("Armut", "Yesil Armut", 12, 5);
+            sepetManager.Ekle2("Elma", "Yesil Elma", 15, 25);
+            sepetManager.Ekle2("Karpuz", "Yesil Karpuz", 80, 15);
+
+        }
+    }
+}
---
+++ Metotlar/SepetManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metotlar
+{
+    public class SepetManager
+    {
+        //namaming convention
+        //syntax
+        public void Ekle(Urun urun)
+        {
+            Console.WriteLine("Teprikler, Sepete " + urun.Adi);
+        }
+
+        public void Ekle2(string urunAdi, string aciklama, double fiyat, double stockadedi)
+        {
+            Console.WriteLine("Teprikler, Sepete " + urunAdi);
+        }
+    }
+}
