ea24b26db564a584251f6dd4edfc0c509728f635
özkan göktaş <karozkan@yahoo.com>
2021-01-23 17:55:10 +0300

    20 Ocak 2021

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        //constructor
+        public MyList() //ctor TabTab
+        {
+            items = new T[10];// array in newlenme zorunluluğu var. array referans tip olduğu için
+        }
+
+        public void Add(T item)
+        {
+            T[] tempArray = items; //Geçici dizi
+            items = new T[items.Length + 1]; //dizi eleman sayısını 1 arttırır
+           
+            for (int i = 0; i < tempArray.Length; i++) //for TabTab
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = item; //items a ekleme burada gerçekleşir
+        }
+    }
+}
---
+++ OOP1/ProductManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    class ProductManager
+    {
+        //Encapsulation
+        public void Add(Product product)
+        {
+            Console.WriteLine("{0} eklendi.", product.ProductName);
+        }
+        public void Update(Product product)
+        {
+            Console.WriteLine("{0} guncellendi.", product.ProductName);
+        }
+        // neden int neden void ?
+        /*
+        public int Topla(int sayi1, int sayi2)
+        {
+            return sayi1 + sayi2;
+        }
+        public void Topla2(int sayi1, int sayi2)
+        {
+            Console.WriteLine(sayi1 + sayi2);
+        }
+        */
+
+    }
+}
