9610f16b3a4af744a965b933400cf78a1299a4b4
özkan göktaş <karozkan@yahoo.com>
2021-01-23 19:03:19 +0300

    4.gün ödev2 videosu

---
+++ Constructor/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Constructor //Constructors (Yapıcı Metodlar). görevi oluşturulan nesneyi ilk kullanıma hazırlamasıdır.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer(){Id=1,FirstName="Özkan",LastName="Göktaş", City="Ankara";
+            
+            Customer customer2 = new Customer(2, "Mehmet", "Abacıoğlu", "Ankara");
+        }
+    }
+    class Customer
+    {
+        public Customer() //defoult constracter
+        {
+        Console.WriteLine("Yapıcı Blok Çalıştı");
+        }
+        public Customer(int id, string firstName, string lastName, string city ) //ctor TabTab
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+        }
+        public int Id { get; set; } //prop TabTab
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+
+    }
+}
