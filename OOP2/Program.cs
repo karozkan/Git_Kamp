c1262861e404bee4ea99c8c3bed8213856a3c6ba
özkan göktaş <karozkan@yahoo.com>
2021-01-23 22:25:39 +0300

    OOP2

---
+++ OOP2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Özkan GÖktaş
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "12345";
+            musteri1.Adi = "Özkan";
+            musteri1.Soyadi = "Göktaş";
+            musteri1.TcNo = "111111111111";
+
+            //Tüzel Müşteri
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "23456";
+            musteri2.SirketAdi = "MEB";
+            musteri2.VergiNo = "1235465464";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+
+
+        }
+    }
+}
---
+++ OOP2/TuzelMusteri.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    // miras inheritance
+
+    class TuzelMusteri:Musteri //Coorporate
+    {
+        public int Id { get; set; }
+        public string MusteriNo { get; set; }
+        public string SirketAdi { get; set; }
+        public string VergiNo { get; set; }
+    }
+}
